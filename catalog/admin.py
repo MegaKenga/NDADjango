ab10ecd5f9f4c0026905cde84c929246d4846b4b
MegaKenga <marka.i09@gmail.com>
2023-08-31 20:48:05 +0300

    admin created

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Brand, Unit, Category, Typeclass, Group, Product
+
+admin.site.register(Brand)
+admin.site.register(Unit)
+admin.site.register(Category)
+admin.site.register(Typeclass)
+admin.site.register(Group)
+admin.site.register(Product)
